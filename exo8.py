83cacbc15c8ac56084f6f3ebe056d82d89a72bd4
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-06-18 21:17:34 +0200

    exo8

---
+++ exo8.py
@@ -0,0 +1,11 @@
+# Exercice 8 : 
+# Modifier la représentation d'une classe
+# Notions abordées : les classes, les méthodes. 
+# Dans cet exercice, nous continuons avec notre classe Voiture. 
+# Le but est de changer la représentation de notre classe afin de pouvoir faire un print 
+# d'une instance de notre classe et avoir des informations précises sur 
+# notre instance. Notre script doit donc retourner la phrase suivante 
+# lorsqu'on print une instance de notre
+# classe : >>> super_voiture = Voiture("lamborghini", 150000, "rouge") >>>
+# print(super_voiture) Votre voiture est une Lamborghini rouge et coûte 150000€
+
